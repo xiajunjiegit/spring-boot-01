19a725ea3672e5e3a7e4ec3e6819acf5f9d325e1
xiajunjiegit <921723915@qq.com>
2021-05-11 17:21:56 +0800

    真正的第二次测试git

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("我是为了测试git。。。。。。");
+        System.out.println("我是为了测试git。。。。。。");
     }
 }
