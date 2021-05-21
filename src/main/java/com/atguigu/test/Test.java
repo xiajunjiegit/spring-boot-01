3553d1749d15940cd65eaea56fe7d84bed170384
imcs <1431844967@qq.com>
2021-05-21 22:24:19 +0800

    测试

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -1,8 +1,19 @@
 package com.atguigu.test;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("我是为了测试git。。。。。。");
-        System.out.println("我是为了测试git。。。。。。");
+        int arr[] = {8, 5, 3, 2, 4};
+        for(int i=0;i<arr.length;i++){
+            for (int j=0;j<arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    int tmp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
